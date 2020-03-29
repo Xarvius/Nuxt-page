ea17c153bc14faea95882a6c06b8798bff54f80a
Xarv <strzelecki20@gmail.com>
2020-03-29 19:54:14 +0200

    Edit: prettier, page host on local network

--- .eslintrc.js
+++ .eslintrc.js
@@ -16,16 +16,5 @@ module.exports = {
   ],
   plugins: ['prettier'],
   // add your custom rules here
-  rules: {
-    'vue/max-attributes-per-line': [
-      2,
-      {
-        singleline: 1,
-        multiline: {
-          max: 1,
-          allowFirstLine: false
-        }
-      }
-    ]
-  }
+
 }
