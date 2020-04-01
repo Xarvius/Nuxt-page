374237a494c3e983936e058eaee1d2daf3abf190
Xarv <strzelecki20@gmail.com>
2020-04-01 15:57:18 +0200

    WIP: Add PvP stats

--- pages/account/Pvp.vue
+++ pages/account/Pvp.vue
@@ -1,14 +1,67 @@
 <template>
-  <div>
-    <h1>About PvP</h1>
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+  <div v-if="stats">
+    <div class="d-flex">
+      <h1 class="mr-auto">PvP stats</h1>
+      <h3 class="p-2">
+        Rank {{ stats.pvp_rank }} x{{ stats.pvp_rank_rollovers }}
+      </h3>
+    </div>
+    <p>Rank points total: {{ stats.pvp_rank_points }}</p>
+    <div class="d-flex">
+      <h3 class="p-2">Total Wins: {{ stats.aggregate.wins }}</h3>
+      <h3 class="p-2">Total Losses: {{ stats.aggregate.losses }}</h3>
+      <h3 class="p-2">Win ratio: {{ totalRatio }}%</h3>
+    </div>
+    <div
+      v-for="[profesion, data] in Object.entries(stats.professions)"
+      :key="profesion"
+    >
+      {{ profesion }}: {{ data }}
+    </div>
   </div>
 </template>
 
 <script>
+import axios from 'axios'
 export default {
-  metaInfo: {
-    title: 'PvP'
+  data() {
+    return {
+      accountAPI: null,
+      stats: null
+    }
+  },
+  computed: {
+    total() {
+      return this.stats.aggregate.wins + this.stats.aggregate.losses
+    },
+    totalRatio() {
+      return ((this.stats.aggregate.wins * 100) / this.total).toFixed(2)
+    }
+  },
+  methods: {
+    pvpAccountStats() {
+      axios
+        .get(
+          `https://cors-anywhere.herokuapp.com/https://api.guildwars2.com/v2/pvp/stats`,
+          {
+            headers: {
+              Authorization: `Bearer ${this.accountAPI}`
+            }
+          }
+        )
+        .then((res) => {
+          this.stats = res.status === 200 ? res.data : null
+          console.log(this.stats)
+        })
+    }
+  },
+  mounted() {
+    if (localStorage.API) {
+      this.accountAPI = localStorage.API
+    } else {
+      window.location.href = '/'
+    }
+    this.pvpAccountStats()
   }
 }
 </script>
